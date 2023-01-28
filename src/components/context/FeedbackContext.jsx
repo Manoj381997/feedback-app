86894b1fa67b6bf12db2dd3bee6d6fecc074addc
Manoj Kumar G <gmanojk84@gmail.com>
2023-01-28 13:36:51 +0530

    Changes related to json mock server

--- src/components/context/FeedbackContext.jsx
+++ src/components/context/FeedbackContext.jsx
@@ -1,16 +1,31 @@
-import { createContext, useState } from 'react';
-import { v4 as uuidv4 } from 'uuid';
-import FeedbackData from '../../data/FeedbackData';
+import { createContext, useState, useEffect } from 'react';
+// import { v4 as uuidv4 } from 'uuid';
 
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
-  const [feedback, setFeedback] = useState(FeedbackData);
+  const [isLoading, setIsLoading] = useState(true);
+  const [feedback, setFeedback] = useState([]);
   const [feedbackEdit, setFeedbackEdit] = useState({ item: {}, edit: false });
 
+  useEffect(() => {
+    fetchFeedback();
+  }, []);
+
+  // Fetch feedbacks from server
+  const fetchFeedback = async () => {
+    const res = await fetch('/feedback?_sort=id&_order=desc');
+    const data = await res.json();
+    setFeedback(data);
+    setIsLoading(false);
+  };
+
   // Delete feedback
-  const deleteFeedback = (id) => {
+  const deleteFeedback = async (id) => {
     if (window.confirm('Are you sure you want to delete?')) {
+      await fetch(`/feedback/${id}`, {
+        method: 'DELETE',
+      });
       setFeedback(feedback.filter((item) => item.id !== id));
     }
   };
@@ -21,16 +36,29 @@ export const FeedbackProvider = ({ children }) => {
   };
 
   // Update feedback
-  const updateFeedback = (id, updItem) => {
+  const updateFeedback = async (id, updItem) => {
+    const res = await fetch(`/feedback/${id}`, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(updItem),
+    });
+    const data = await res.json();
     setFeedback(
-      feedback.map((item) => (item.id === id ? { ...item, ...updItem } : item))
+      feedback.map((item) => (item.id === id ? { ...item, ...data } : item))
     );
   };
 
   // Add feedback
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4();
-    setFeedback([newFeedback, ...feedback]);
+  const addFeedback = async (newFeedback) => {
+    const res = await fetch('/feedback', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(newFeedback),
+    });
+    const data = await res.json();
+
+    // newFeedback.id = uuidv4(); // To generate unique ID
+    setFeedback([data, ...feedback]);
   };
 
   return (
@@ -38,6 +66,7 @@ export const FeedbackProvider = ({ children }) => {
       value={{
         feedback,
         feedbackEdit,
+        isLoading,
         deleteFeedback,
         addFeedback,
         editFeedback,
